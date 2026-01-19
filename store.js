7d84b68e49bc4c1832685400bbd98e21f9610b81
Server <yanis.messadditmahtal01@gmail.com>
2026-01-19 16:05:52 +0100

    Add files via upload

---
+++ store.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.buy-now-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        window.location.href = 'https://discord.gg/gydunWJX';
+    });
+});
